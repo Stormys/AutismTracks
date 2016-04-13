136f2bae8ee89caf39876d77aa11838ec8f7ca87
Julian Tuminaro <julian.tuminaro@gmail.com>
2016-04-13 15:53:58 -0700

    More Done

---
+++ app/src/main/java/jt/autismtracks/HomePage.java
@@ -0,0 +1,48 @@
+package jt.autismtracks;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class HomePage extends ListActivity {
+
+    private ArrayList<String> values = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        create_adapter();
+        insert_button();
+    }
+
+    private void insert_button() {
+        Button button = (Button) findViewById(R.id.NewTask);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, TaskSettings.class);
+                startActivityForResult(intent,2);
+            }
+        });
+    }
+
+    private void create_adapter() {
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+    }
+
+    protected void onActivityResult(int requestedCode, int resultCode, Intent data) {
+        if(requestedCode==2) {
+            adapter.add(data.getStringExtra("Title"));
+        }
+    }
+}
---
+++ app/src/main/java/jt/autismtracks/TaskSettings.java
@@ -0,0 +1,78 @@
+package jt.autismtracks;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import java.util.Calendar;
+
+public class TaskSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_settings);
+        create_toolbar();
+        create_submit_button();
+    }
+
+    protected void create_toolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("New Task");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void create_submit_button() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                EditText temp = (EditText) findViewById(R.id.new_task);
+                i.putExtra("Title", temp.getText().toString());
+                setResult(2,i);
+                finish();
+            }
+        });
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        public static final String[] MONTHS = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            TextView tvDate = (TextView) getActivity().findViewById(R.id.date);
+            tvDate.setText(MONTHS[month] + " " + String.valueOf(day) + ", " + String.valueOf(year));
+        }
+    }
+}
