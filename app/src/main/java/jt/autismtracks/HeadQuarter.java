39e30ef0109b2f691214b2a6445382c2f91fc351
Julian Tuminaro <julian.tuminaro@gmail.com>
2016-04-18 16:14:14 -0700

    HomePage Support for a lot of things

--- app/src/main/java/jt/autismtracks/HeadQuarter.java
+++ app/src/main/java/jt/autismtracks/HeadQuarter.java
@@ -1,17 +1,23 @@
 package jt.autismtracks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.internal.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -21,6 +27,8 @@ public class HeadQuarter extends AppCompatActivity {
     private ArrayList<String> items = new ArrayList<String>();
     private FloatingActionButton fab, fab1, fab2;
     private Animation rotate_forward, rotate_backwards, fab_open, fab_close;
+    private TextView add_task_tv, add_goals_tv;
+    private ListView lv;
     private boolean isClicked = false;
 
     @Override
@@ -29,10 +37,11 @@ public class HeadQuarter extends AppCompatActivity {
         setContentView(R.layout.activity_head_quarter);
         set_list_adapter();
         set_fab_clicker();
+        set_task_list_clicker();
     }
 
     private void set_list_adapter() {
-        ListView lv = (ListView) findViewById(R.id.listView);
+        lv = (ListView) findViewById(R.id.listView);
         items.add("Task List");
         items.add("Goals");
         items.add("Rewards");
@@ -49,6 +58,8 @@ public class HeadQuarter extends AppCompatActivity {
         rotate_backwards = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backwards);
         fab_open = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
         fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        add_task_tv = (TextView) findViewById(R.id.add_task_tv);
+        add_goals_tv = (TextView) findViewById(R.id.add_goal_tv);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +68,8 @@ public class HeadQuarter extends AppCompatActivity {
                     fab.startAnimation(rotate_backwards);
                     fab1.startAnimation(fab_close);
                     fab2.startAnimation(fab_close);
+                    add_task_tv.startAnimation(fab_close);
+                    add_goals_tv.startAnimation(fab_close);
                     fab1.setClickable(false);
                     fab2.setClickable(false);
                     isClicked = false;
@@ -64,6 +77,8 @@ public class HeadQuarter extends AppCompatActivity {
                     fab.startAnimation(rotate_forward);
                     fab1.startAnimation(fab_open);
                     fab2.startAnimation(fab_open);
+                    add_task_tv.startAnimation(fab_open);
+                    add_goals_tv.startAnimation(fab_open);
                     fab1.setClickable(true);
                     fab2.setClickable(true);
                     isClicked = true;
@@ -71,5 +86,22 @@ public class HeadQuarter extends AppCompatActivity {
             }
 
         });
+    }
+
+    private void set_task_list_clicker() {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(HeadQuarter.this, HomePage.class);
+                    startActivity(intent);
+                } else if (position == 1) {
+                    Intent intent = new Intent(HeadQuarter.this, Goals.class);
+                    startActivity(intent);
+                } else if (position == 2) {
+                    Intent intent = new Intent(HeadQuarter.this, Rewards.class);
+                    startActivity(intent);
+                }
+            }
+        } );
     }
 }
